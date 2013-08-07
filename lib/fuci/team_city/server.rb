35a09adbd48cc26d07109d190303ed67d0c72ac4
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-08-07 18:49:31 -0400

    Impl Server#build_status

--- lib/fuci/team_city/server.rb
+++ lib/fuci/team_city/server.rb
@@ -1,6 +1,11 @@
+require 'forwardable'
+
 module Fuci
   module TeamCity
     class Server < Fuci::Server
+      extend Forwardable
+
+      def_delegator :build, :status, :build_status
     end
   end
 end
--- spec/lib/fuci/team_city/server_spec.rb
+++ spec/lib/fuci/team_city/server_spec.rb
@@ -3,10 +3,27 @@ stub_class 'Fuci::Server'
 require_relative '../../../../lib/fuci/team_city/server'
 
 describe Fuci::TeamCity::Server do
+  before do
+    @server = Fuci::TeamCity::Server.new
+  end
+
   describe 'composition' do
     it 'inherits from Fuci::Server' do
-      server = Fuci::TeamCity::Server.new
-      expect(server).to_be_kind_of Fuci::Server
+      expect(@server).to_be_kind_of Fuci::Server
+    end
+  end
+
+  describe '#build_status' do
+    it 'delegates to #build' do
+      @server.stubs(:build).returns build = mock
+      build.stubs(:status).returns build_status = mock
+
+      expect(@server.build_status).to_equal build_status
+    end
+  end
+
+  describe '#fetch_log' do
+    it 'delegates to #build' do
     end
   end
 end
