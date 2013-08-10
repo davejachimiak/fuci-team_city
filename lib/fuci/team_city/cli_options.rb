e131ec121ab9bedff8d78eb642e76caf40761977
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-08-10 11:44:46 -0400

    Delegate .argv to Fuci::CliOptions

--- lib/fuci/team_city/cli_options.rb
+++ lib/fuci/team_city/cli_options.rb
@@ -1,8 +1,16 @@
+require 'fuci/cli_options'
+
 module Fuci
   module TeamCity
     class CliOptions
       def self.branch
         argv.first
+      end
+
+      private
+
+      def self.argv
+        Fuci::CliOptions.argv
       end
     end
   end
@@ -28,6 +36,14 @@ describe Fuci::TeamCity::CliOptions do
       it 'returns nil' do
         expect(Fuci::TeamCity::CliOptions.branch).to_be_nil
       end
+    end
+  end
+
+  describe '.argv' do
+    it 'delegates to Fuci::CliOptions' do
+      Fuci::CliOptions.stubs(:argv).returns argv = ['an option']
+
+      expect(Fuci::TeamCity::CliOptions.argv).to_equal argv
     end
   end
 end
